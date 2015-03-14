9224a658ec9e9ea1fe41405ddcffb1bc6d070e5f
maxtmn <mastodont1984@gmail.com>
2015-03-15 01:56:19 +0200

    Revert "Revert "add rest-api for working with activiti""
    This reverts commit a9fdb907381cc0d7c620966fd724a1c2031f3bab.

---
+++ wf-dniprorada/src/main/java/org/activiti/rest/controller/ActivitiRestApiController.java
@@ -0,0 +1,57 @@
+package org.activiti.rest.controller;
+
+import java.util.List;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * ...wf-dniprorada/service/...
+ * Example:
+ * http://localhost:9090/wf-dniprorada/service/startProcessByKey/citizensRequest
+ * 
+ * @author Tereshchenko
+ *
+ */
+@Controller
+@Service
+public class ActivitiRestApiController {
+	private final Logger log = LoggerFactory
+			.getLogger(ActivitiRestApiController.class);
+
+	@Autowired
+	private RuntimeService runtimeService;
+
+	@Autowired
+	private TaskService taskService;
+
+	@RequestMapping(value = "/startProcessByKey/{key}", method = RequestMethod.GET)
+	@Transactional
+	public @ResponseBody String startProcess(@PathVariable("key") String key) {
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey(key);
+		if (pi == null || pi.getId() == null) {
+			throw new IllegalArgumentException(String.format(
+					"process did not started by key:{%s}", key));
+		}
+		return pi.getProcessInstanceId();
+	}
+
+	@RequestMapping(value = "/getTasks/{assignee}", method = RequestMethod.GET)
+	@Transactional
+	public List<Task> getTasks(@PathVariable("assignee") String assignee) {
+		return taskService.createTaskQuery().taskAssignee(assignee).list();
+	}
+
+}
