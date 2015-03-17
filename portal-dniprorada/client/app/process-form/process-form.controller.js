744b1ac599d751ffa3b07e0442385d43893cc5ff
domash <aleksej.domash@gmail.com>
2015-03-17 23:44:24 +0200

    now we can show initial form for process starting (without validations, and types of fields)

--- portal-dniprorada/client/app/process-form/process-form.controller.js
+++ portal-dniprorada/client/app/process-form/process-form.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('portalDniproradaApp')
-	.controller('ProcessFormCtrl', function($scope, $routeParams, $http) {
+	.controller('ProcessFormCtrl', function($scope, $routeParams, $http, $window) {
 		$scope.processDefinitionId = $routeParams.processDefinitionId;
+		$scope.startProcess = function(form) {
+			$window.alert("Should call process started with " + form.toString());
+		};
 		$http.get('/api/process-form/' + $routeParams.processDefinitionId)
 			.success(function(result) {
 				$scope.processFormData = result;
