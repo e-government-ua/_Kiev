7cf8a4775d00e852909c1978f5a616b2c12c6346
Inna <inna.dascenko@privatbank.ua>
2015-03-30 20:13:44 +0300

    fix error
    Signed-off-by: Inna <inna.dascenko@privatbank.ua>

--- wf-dniprorada/src/main/java/org/wf/dp/dniprorada/engine/task/MailTaskWithAttachment.java
+++ wf-dniprorada/src/main/java/org/wf/dp/dniprorada/engine/task/MailTaskWithAttachment.java
@@ -60,7 +60,7 @@ public class MailTaskWithAttachment implements JavaDelegate {
 		email.setSubject(subjectStr);
 		email.setMsg(textStr);
 		email.setAuthentication(mailServerUsername, mailServerPassword);
-		email.setSmtpPort(mailServerPort);
+		email.setSmtpPort(Integer.valueOf(mailServerPort));
 		email.setSSL(true);
 		email.setTLS(true);
 		email.attach(emailAttachment);
