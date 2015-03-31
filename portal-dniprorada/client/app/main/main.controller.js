5c1027232c7be340460462355fde6736b108731e
hudson-206 <hudson-206>
2015-03-31 19:19:07 +0300

    https://github.com/e-government-ua/dniprorada/issues/8 последние версии++ /rest/getProcessDefinitions

--- portal-dniprorada/client/app/main/main.controller.js
+++ portal-dniprorada/client/app/main/main.controller.js
@@ -4,8 +4,8 @@ angular.module('portalDniproradaApp')
 	.controller('MainCtrl', function($scope, $http) {
 		$scope.processDefinitions = [];
 
-		/*$http.get('/api/process-definitions').success(function(result) {*/
-		$http.get('/rest/getProcessDefinitions').success(function(result) {
+		$http.get('/api/process-definitions').success(function(result) {
+		/*$http.get('/rest/getProcessDefinitions').success(function(result) {*/
 			$scope.processDefinitions = result.data;
                         /*if(result.data.length>0){
                             $scope.processDefinitions = [result.data[result.data.length-1]];
