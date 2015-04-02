c4cac30a19ae04c7f710a6a39b5777b0d5a1a9d1
Softer <softer@lin.in.ua>
2015-04-02 17:58:06 +0300

    Test fix

--- conf.php
+++ conf.php
@@ -1,6 +1,4 @@
 <?php
-
-define('activityUrl','https://52.17.126.64:8080/wf-dniprorada/service',false);
-define('selfUrl','http://52.17.126.64:5588',false);
-
+  define('activityUrl','https://52.17.126.64:8080/wf-dniprorada/service',false);
+  define('selfUrl','http://52.17.126.64:5588',false);
 ?>
