3407f189feab14601af088420b8ebee981df4597
yevheniilazarev <yevheniilazarev@privatbank.ua>
2015-04-18 23:04:45 +0300

    change RequestMethod in AuthController to POST

--- wf-dniprorada/src/main/java/org/egov/web/controller/AuthController.java
+++ wf-dniprorada/src/main/java/org/egov/web/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.wf.dp.dniprorada.service.ProcessingUser;
 
@@ -14,11 +15,10 @@ public class AuthController {
 	@Autowired
 	ProcessingUser procUser;
 
-	@RequestMapping(value = "/params/{sLogin}/{sPassword}", method = RequestMethod.GET)
-	public @ResponseBody boolean getUserNameAndPassword(@PathVariable String sLogin,
-			@PathVariable String sPassword) {
+	@RequestMapping(value = "/params", method = RequestMethod.POST)
+	public @ResponseBody boolean getUserNameAndPassword(@RequestParam(value = "param") String authParam[]) {
 
-		if (procUser.validateUser(sLogin, sPassword)) {
+		if (procUser.validateUser(authParam[0], authParam[1])) {
 			//create and return session
 			return true;
 		} else {
