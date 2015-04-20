8b48686d1a07648260138fe273b23ef631c02dbb
Inna <inna.dascenko@privatbank.ua>
2015-04-20 13:46:18 +0300

    remove log4j
    Signed-off-by: Inna <inna.dascenko@privatbank.ua>

--- redis/src/main/java/org/activiti/redis/config/RedisConfiguration.java
+++ redis/src/main/java/org/activiti/redis/config/RedisConfiguration.java
@@ -2,12 +2,7 @@ package org.activiti.redis.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Настройки Редис.
