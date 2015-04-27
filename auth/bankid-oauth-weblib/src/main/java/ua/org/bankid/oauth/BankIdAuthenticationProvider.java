36a94bafc2aaeed41290c5b98493357e1a9ee311
zora-borys <zora.borys@javatar.com.ua>
2015-04-27 23:57:26 +0300

    issue #37: refactor oauth configuration and fix some groovy tests

--- auth/bankid-oauth-weblib/src/main/java/ua/org/bankid/oauth/BankIdAuthenticationProvider.java
+++ auth/bankid-oauth-weblib/src/main/java/ua/org/bankid/oauth/BankIdAuthenticationProvider.java
@@ -1,29 +1,26 @@
 package ua.org.bankid.oauth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.stereotype.Service;
-import ua.org.bankid.oauth.BankIdStorageService;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 /**
  * @author zora.borys
  */
-@Service("authenticationProvider")
 public class BankIdAuthenticationProvider implements AuthenticationProvider {
-
-    @Autowired
-    private BankIdStorageService bankIdStorageService;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return bankIdStorageService.authenticate(authentication);
+        return null; // TODO
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+        return  OAuth2Authentication.class.isAssignableFrom(authentication) ||
+                PreAuthenticatedAuthenticationToken.class.isAssignableFrom(authentication) ||
+                UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
     }
 }
