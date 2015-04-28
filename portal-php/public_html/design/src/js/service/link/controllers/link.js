3068d4da219a6ad56bbaf4b56946e786446347dc
Artur Kvasnii <artur.kvasnii@gmail.com>
2015-04-28 22:29:05 +0300

    Добавив можливість переходу до кроку номер 2.

--- portal-php/public_html/design/src/js/service/link/controllers/link.js
+++ portal-php/public_html/design/src/js/service/link/controllers/link.js
@@ -1,4 +1,8 @@
 define('service/link/controller', ['angularAMD'], function (angularAMD) {
-	angularAMD.controller('ServiceLinkController', ['$rootScope', function ($rootScope) {
+	angularAMD.controller('ServiceLinkController', ['$rootScope', '$scope', function ($rootScope, $scope) {
+		$scope.data = {
+			region: null,
+			city: null
+		};
     }]);
 });
