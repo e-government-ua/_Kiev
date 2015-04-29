8d4f7a401a06fd94b7c7d8d246d130a8f46c1ce4
ValerijMakarenko <Valerij.Makarenko@privatbank.ua>
2015-04-29 12:12:50 +0300

    test

--- wf-dniprorada/src/test/java/org/activiti/test/bp/dnepr/mvk_1/Mvk_1_Test.java
+++ wf-dniprorada/src/test/java/org/activiti/test/bp/dnepr/mvk_1/Mvk_1_Test.java
@@ -24,7 +24,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.plexiti.activiti.test.fluent.ActivitiFluentTestHelper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ActiveProfiles("default")
+@ActiveProfiles("default")
 @ContextConfiguration(locations = {
 		"classpath:META-INF\spring\dnepr\mvk\1\activiti.cfg.mock.xml",
 		"classpath:META-INF\spring\dnepr\mvk\1\activiti.cfg.scan.xml",
