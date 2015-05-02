6477c2d7c9087d5b6babe0076738684816af99bb
sybase <bw@pb.ua>
2015-05-02 23:44:33 +0300

    исправлен и не транслируемые названия в списке задач https://github.com/e-government-ua/i/issues/145

--- dashboard-js/server/api/process/process.controller.js
+++ dashboard-js/server/api/process/process.controller.js
@@ -4,8 +4,13 @@ var activiti = require('../../components/activiti');
 
 // Get list of processs
 exports.index = function(req, res) {
+    
+        var query = {};
+        query.size = 100;
+    
 	var options = {
 		path: 'repository/process-definitions'
+                , query: query
 	};
 
 	activiti.get(options, function(error, statusCode, result) {
