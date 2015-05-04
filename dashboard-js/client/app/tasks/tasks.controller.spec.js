42d7a90812d4665b4e93a614256c42d333f72114
Daniil <daniil@redmart.com>
2015-05-05 00:22:02 +0800

    Fix tests.

--- dashboard-js/client/app/tasks/tasks.controller.spec.js
+++ dashboard-js/client/app/tasks/tasks.controller.spec.js
@@ -1,28 +1,38 @@
 'use strict';
 describe('Controller: TasksCtrl', function() {
-	var scope, tasks, q;
+  var scope, tasks, q, processes;
 
-	beforeEach(module('dashboardJsApp'));
+  beforeEach(module('dashboardJsApp'));
+  beforeEach(inject(function($controller, $rootScope, $q) {
+    scope = $rootScope.$new();
+    q = $q;
+    mockTasksService();
+    mockProcessesService();
+    createController($controller);
+  }));
 
-	beforeEach(inject(function($controller, $rootScope, $q) {
-		scope = $rootScope.$new();
-		q = $q;
-		mockTasksService();
-		$controller('TasksCtrl', {
-			$scope: scope,
-			tasks: tasks
-		});
-	}));
+  it('provides selected task', function() {
+    scope.selectedTask = {id: '666'};
+    expect(scope.isTaskSelected({id: '666'})).toBeTruthy();
+    expect(scope.isTaskSelected({id: 666})).toBeFalsy();
+  });
 
-	it('provides selected task', function() {
-		scope.selectedTask = {id: '666'};
-		expect(scope.isTaskSelected({id: '666'})).toBeTruthy();
-		expect(scope.isTaskSelected({id: 666})).toBeFalsy();
-	});
+  function mockTasksService () {
+    tasks = jasmine.createSpyObj('task', ['list']);
+    tasks.list.andReturn(q.when({}));
+    tasks.filterTypes = {};
+  }
 
-	function mockTasksService () {
-		tasks = jasmine.createSpyObj('task', ['list']);
-		tasks.list.andReturn(q.when({}));
-		tasks.filterTypes = {};
-	}
+  function mockProcessesService () {
+    processes = jasmine.createSpyObj('processes', ['list']);
+    processes.list.andReturn(q.when({}));
+  }
+
+  function createController ($controller) {
+    $controller('TasksCtrl', {
+      $scope: scope,
+      tasks: tasks,
+      processes: processes
+    });
+  }
 });
