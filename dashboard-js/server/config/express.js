501f34289b8e37f0320d9aa71fab8a8c0d08e2ac
domash <aleksej.domash@gmail.com>
2015-05-11 21:24:27 +0300

    #177 correct timestamp format, format for morgan logger

--- dashboard-js/server/config/express.js
+++ dashboard-js/server/config/express.js
@@ -38,7 +38,7 @@ module.exports = function(app) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
-    app.use(morgan('dev'));
+    app.use(morgan(':method :url :status :response-time ms - :res[content-length]'));
   }
 
   if ('development' === env || 'test' === env) {
