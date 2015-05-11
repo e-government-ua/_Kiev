3d91d18a1a6ee60bbf7237ec89bcbe1b615032ab
Maxim Shekhovets <maxymshg@gmail.com>
2015-05-11 20:42:43 +0300

    init states for authorization via BankID
    - initial state of dirty solution for authorization via BankID
    - cannot get access_token
    - BankIdService not worked for me...

--- central-js/client/src/js/documents/controllers/states.js
+++ central-js/client/src/js/documents/controllers/states.js
@@ -1,5 +1,5 @@
 define('state/documents/controller', ['angularAMD'], function (angularAMD) {
-	angularAMD.controller('DocumentsController', ['$rootScope','$scope', function ($rootScope, $scope) {
+	angularAMD.controller('DocumentsController', ['$rootScope', '$state', function ($rootScope, $state) {
 		console.log('$rootScope');
 		var documents = [
 			{
@@ -61,5 +61,32 @@ define('state/documents/controller', ['angularAMD'], function (angularAMD) {
 		];
 
 		$scope.documents = documents;
+
+		console.log('DocumentsController');
+        if (!$state.params.code) {
+            $state.go('documents.bankid', {}, { location: true });
+        }
+    }]);
+});
+define('state/documents/bankid/controller', ['angularAMD'], function (angularAMD) {
+	angularAMD.controller('DocumentsBankIdController', ['$rootScope', '$scope', '$location', '$state', '$window', function ($rootScope, $scope, $location, $state, $window) {
+		console.log('$rootScope');
+        console.log('DocumentsBankIdController');
+        var stateForRedirect = $state.href('documents.content', {});
+        var redirectURI = $location.protocol() + '://' + $location.host() + ':' + $location.port() + stateForRedirect;
+        var bankIdLoginRefUrl = 'https://bankid.privatbank.ua/DataAccessService/das/authorize?response_type=code&client_id=dniprorada&redirect_uri=' + redirectURI;
+        $scope.loginWithBankId = function () {
+            $window.location.href = bankIdLoginRefUrl;
+        }
+    }]);
+});
+define('state/documents/content/controller', ['angularAMD'], function (angularAMD) {
+	angularAMD.controller('DocumentsContentController', [
+        '$rootScope', '$scope', '$state', '$stateParams', 'BankIDLogin',
+        function ($rootScope, $scope, $state, $stateParams, BankIDLogin) {
+		console.log('$rootScope');
+        console.log('DocumentsContentController');
+        console.log($stateParams);
+        console.log(BankIDLogin);
     }]);
 });
