fd1030b131b3d146ca0964068a709dde55e30b96
sybase <bw@pb.ua>
2015-05-13 02:26:37 +0300

    https://github.com/e-government-ua/i/issues/208 +

--- central-js/client/src/js/service/city/controllers/states.js
+++ central-js/client/src/js/service/city/controllers/states.js
@@ -4,6 +4,28 @@ define('state/service/city/controller', ['angularAMD'], function (angularAMD) {
 			$scope.service = service;
 			$scope.places = places;
 			
+                        $scope.sFieldLabel = function(sField) {
+                          var s="";
+                          if (sField !== null) {
+                            var a=sField.split(";");
+                            s=a[1].trim();
+                          }
+                          return s;
+                        };
+                        $scope.sFieldNotes = function(sField) {
+                          var s=null;
+                          if (sField !== null) {
+                            var a=sField.split(";");
+                            if(a.length>1){
+                              s=a[1].trim();
+                              if(s==""){
+                                  s=null;
+                              }
+                            }
+                          }
+                          return s;
+                        };                    
+                        
 			$scope.data = {
 				region: null,
 				city: null
