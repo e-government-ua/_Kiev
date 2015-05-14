175f0406f19f5f34f73c5267feb21d9580ebad8f
Artur Kvasnii <artur.kvasnii@gmail.com>
2015-05-14 11:04:08 +0300

    test api/bankid/login

--- central-js/server/api/bankid/login.controller.js
+++ central-js/server/api/bankid/login.controller.js
@@ -2,18 +2,7 @@ var request = require('request');
 
 module.exports.index = function(res,options, callback) {
 	var url = options.protocol+'://'+options.hostname+options.path+'/oauth/token';
-	res.send({
-		'url': url,
-		'qs': {
-			'grant_type': 'authorization_code',
-			'client_id': options.params.client_id,
-			'client_secret': options.params.client_secret,
-			'code': options.params.code,
-			'redirect_uri': options.params.redirect_uri
-		}
-	});
-	res.end();
-	/*
+
 	return request.get({
 		'url': url,
 		'qs': {
@@ -23,6 +12,9 @@ module.exports.index = function(res,options, callback) {
 			'code': options.params.code,
 			'redirect_uri': options.params.redirect_uri
 		}
-	}, callback);
-	*/
+	}, function(error, response, body) {
+		res.send(error);
+		res.end();
+	});
+
 };
