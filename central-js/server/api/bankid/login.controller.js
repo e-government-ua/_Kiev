c2e153f6eed356abb4a1076b52cfae1e4c91e52c
Artur Kvasnii <artur.kvasnii@gmail.com>
2015-05-14 11:33:05 +0300

    test api/bankid/login

--- central-js/server/api/bankid/login.controller.js
+++ central-js/server/api/bankid/login.controller.js
@@ -11,7 +11,8 @@ module.exports.index = function(res,options, callback) {
 			'client_secret': options.params.client_secret,
 			'code': options.params.code,
 			'redirect_uri': options.params.redirect_uri
-		}
+		},
+		'timeout': 5000
 	}, function(error, response, body) {
 		res.send(error);
 		res.end();
