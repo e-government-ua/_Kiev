a4457d7aceaebc8ca5bc4ac7f61b2f65868fe51e
yevheniilazarev <evgenij.lazarev@privatbank.ua>
2015-05-18 19:43:15 +0300

    [issue#198] service document access

---
+++ wf-central/src/main/java/org/wf/dp/dniprorada/model/SURL.java
@@ -0,0 +1,18 @@
+package org.wf.dp.dniprorada.model;
+
+public class SURL {
+	private String name;
+	private String value;
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
