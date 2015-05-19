145c2fc474d00f2ce1f4fae99d1a3e36f3468866
yevheniilazarev <evgenij.lazarev@privatbank.ua>
2015-05-19 15:19:10 +0300

    issue#198 change name filed

--- wf-central/src/main/java/org/wf/dp/dniprorada/dao/DocumentAccessDaoImpl.java
+++ wf-central/src/main/java/org/wf/dp/dniprorada/dao/DocumentAccessDaoImpl.java
@@ -73,7 +73,7 @@ public class DocumentAccessDaoImpl implements DocumentAccessDao {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 		jdbcTemplate
 				.update("INSERT INTO DocumentAccess"
-						+ " (nID_Document, sDateCreate, sMS, sFIO, sTarget, sTelephone, sMail, "
+						+ " (nID_Document, sDateCreate, nMS, sFIO, sTarget, sTelephone, sMail, "
 						+ "sSecret) VALUES (?,?,?,?,?,?,?,?)",
 						da.getnID_Document(), da.getsDateCreate(),
 						da.getsDays(), da.getsFIO(), da.getsTarget(),
@@ -82,9 +82,14 @@ public class DocumentAccessDaoImpl implements DocumentAccessDao {
 
 	private Integer getIdAccess() {
 		jdbcTemplate = new JdbcTemplate(dataSource);
-		DocumentAccess da = jdbcTemplate
+		DocumentAccess da = null;
+		try{
+		da = jdbcTemplate
 				.query("SELECT nID FROM DocumentAccess order by nID desc limit 1",
 						new DocumentAccessRowMapper()).get(0);
+		} catch(Exception e){
+			da.setnID(0);
+		}
 		return da.getnID();
 	}
 
@@ -93,7 +98,7 @@ public class DocumentAccessDaoImpl implements DocumentAccessDao {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 		List <DocumentAccess> listDa = jdbcTemplate
 				.query("SELECT nID, nID_Document, "
-						+ "sDateCreate, sMS, sFIO, sTarget, sTelephone, sMail, sSecret "
+						+ "sDateCreate, nMS, sFIO, sTarget, sTelephone, sMail, sSecret "
 						+ "FROM DocumentAccess "
 						+ "WHERE nID=? AND sSecret=?",
 						new DocumentAccessRowMapper(), nID_Access, sSecret);
