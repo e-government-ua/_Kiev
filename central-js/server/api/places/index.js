c05675b600b715755b03be8f7e729f4a5618736f
Sergey P <spamazure@mail.ru>
2015-05-21 21:43:06 +0300

    wip: cache for places on nodeside. todo: nodeside filtering of places list by initials letter from client. client: wait for at least 2 chars to query node

--- central-js/server/api/places/index.js
+++ central-js/server/api/places/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var NodeCache = require( "node-cache" );
+var myCache = new NodeCache();
 
 router.use(function(req, res, next) {
+
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 	var places = require('./index.controller');
-	
 	var config = require('../../config');
 	var activiti = config.activiti;
 	
@@ -16,13 +18,28 @@ router.use(function(req, res, next) {
 		username: activiti.username,
 		password: activiti.password
 	};
+
+	var cacheKey = 'api/places';
 	
 	var callback = function(error, response, body) {
+		// cache successful answer for 1 day (86400 seconds)
+		if (error === null) {
+			myCache.set(cacheKey, body, 86400);
+		} else {
+			// clear cache in case of error
+			myCache.del(cacheKey);
+		}
 		res.send(body);
 		res.end();
 	}
+	// use cached value if available, otherwise make an index call
+	if (myCache.get(cacheKey) !== undefined) {
+		res.send(myCache.get(cacheKey));
+		res.end();
+	} else {
+		places.index(options, callback);
+	}
 	
-	places.index(options, callback);
 });
 
 module.exports = router;
