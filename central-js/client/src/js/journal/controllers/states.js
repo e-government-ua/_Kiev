676be42ed1f1af358ad7c45c431941d9d93a4e1a
sybase <bw@pb.ua>
2015-05-23 01:58:35 +0300

    https://github.com/e-government-ua/i/issues/273

--- central-js/client/src/js/journal/controllers/states.js
+++ central-js/client/src/js/journal/controllers/states.js
@@ -1,5 +1,51 @@
-define('state/journal/controller', ['angularAMD'], function (angularAMD) {
+/*define('state/journal/controller', ['angularAMD'], function (angularAMD) {
 	angularAMD.controller('JournalController', ['$rootScope', function ($rootScope) {
 		console.log('$rootScope');
+    }]);
+});*/
+
+define('state/journal/controller', ['angularAMD'], function (angularAMD) {
+	angularAMD.controller('JournalController', ['$rootScope', '$state', '$window', '$location', function ($rootScope, $state, $window, $location) {
+		console.log('$rootScope');
+		console.log('JournalController');
+        console.log($state.current);
+        if ($state.is('journal.bankid') && !!$state.params.code) {
+            console.log('journal.content');
+            $window.location.href = $location.protocol()
+                + '://'
+                + $location.host()
+                + ':'
+                + $location.port()
+                + $state.href('journal.content', {code: $state.params.code});
+        }
+    }]);
+});
+
+define('state/journal/bankid/controller', ['angularAMD'], function (angularAMD) {
+	angularAMD.controller('JournalBankIdController', ['$rootScope', '$scope', '$location', '$state', '$window', function ($rootScope, $scope, $location, $state, $window) {
+		console.log('$rootScope');
+        console.log('JournalBankIdController');
+
+        $scope.loginWithBankId = function () {
+            var stateForRedirect = $state.href('journal.bankid', {});
+            var redirectURI = $location.protocol() + '://' + $location.host() + ':' + $location.port() + stateForRedirect;
+            $window.location.href = 'https://bankid.privatbank.ua/DataAccessService/das/authorize?response_type=code&client_id=dniprorada&redirect_uri=' + redirectURI;
+        }
+    }]);
+});
+define('state/journal/content/controller', ['angularAMD'], function (angularAMD) {
+	angularAMD.controller('JournalContentController', [
+        '$rootScope', '$scope', '$state', 'journal',
+        function ($rootScope, $scope, $state, journal) {
+		console.log('$rootScope');
+        console.log('JournalContentController');
+        console.log($state);
+        console.log(journal);
+        /*angular.forEach(journal, function (item) {
+            if (item.oDate_Upload === null) {
+                item.oDate_Upload = new Date();
+            }
+        });*/
+        $scope.journal = journal;
     }]);
 });
--- central-js/client/src/js/journal/script.js
+++ central-js/client/src/js/journal/script.js
@@ -19,3 +19,82 @@ define('journal', ['angularAMD'], function (angularAMD) {
     return app;
 });
 
+define('journal', ['angularAMD', 'service'], function (angularAMD) {
+    var app = angular.module('journal', []);
+
+    app.config(['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('journal', {
+                url: '/journal',
+                views: {
+                    '': angularAMD.route({
+                        templateProvider: ['$templateCache', function($templateCache) {
+							return $templateCache.get('html/journal/index.html');
+						}],
+						controller: 'JournalController',
+                        controllerUrl: 'state/journal/controller'
+                    })
+                }
+            })
+            .state('journal.bankid', {
+                url: '/bankid?code',
+                parent: 'journal',
+                views: {
+                    'bankid': angularAMD.route({
+                        templateProvider: ['$templateCache', function($templateCache) {
+							return $templateCache.get('html/journal/bankid/index.html');
+						}],
+						controller: 'JournalBankIdController',
+                        controllerUrl: 'state/journal/bankid/controller'
+                    })
+                }
+            })
+            .state('journal.content', {
+                url: '/content?code',
+                parent: 'journal',
+                resolve: {
+                    BankIDLogin: ['$q', '$state', '$location', '$stateParams', 'BankIDService', function($q, $state, $location, $stateParams, BankIDService) {
+                        var url = $location.protocol()
+                            +'://'
+                            +$location.host()
+                            +':'
+                            +$location.port()
+                            +$state.href('journal.bankid');
+
+                        return BankIDService.login($stateParams.code, url).then(function(data) {
+                            return data.hasOwnProperty('error') ? $q.reject(null): data;
+                        });
+                    }],
+                    BankIDAccount: ['BankIDService', 'BankIDLogin', function(BankIDService, BankIDLogin) {
+                        return BankIDService.account(BankIDLogin.access_token);
+                    }],
+                    customer: ['BankIDAccount', function (BankIDAccount) {
+                        return BankIDAccount.customer;
+                    }],
+                    subject: ['$q', '$state', 'ServiceService', 'customer', function($q, $state, ServiceService, customer) {
+                        $state.customer = customer;
+                        return ServiceService.syncSubject(customer.inn).then(function(data) {
+                            return data.hasOwnProperty('error') ? $q.reject(null): data;
+                        });
+                    }],
+                    journal: ['$q', '$state', 'subject', 'ServiceService', function($q, $state, subject, ServiceService) {
+                        $state.nID_Subject = subject.nID;
+                        /*return ServiceService.getDocuments($state.nID_Subject).then(function(data) {
+                            return data.hasOwnProperty('error') ? $q.reject(null) : data;
+                        });*/
+                    }]
+                },
+                views: {
+                    'content': angularAMD.route({
+                        templateProvider: ['$templateCache', function($templateCache) {
+							return $templateCache.get('html/journal/content.html');
+						}],
+						controller: 'JournalContentController',
+                        controllerUrl: 'state/journal/content/controller'
+                    })
+                }
+            })
+    }]);
+    return app;
+});
+
