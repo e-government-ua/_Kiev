ad9126ffc27223021ddd6e9b64219eb9931408cb
sybase <bw@pb.ua>
2015-05-25 00:22:57 +0300

    test processDefinitionId.sProcessDefinitionKeyWithVersion; +ActivitiForm.sProcessDefinitionName

--- central-js/client/src/js/form/factories/formData.js
+++ central-js/client/src/js/form/factories/formData.js
@@ -17,6 +17,7 @@ define('formData/factory', ['angularAMD', 'file/directive', 'parameter/factory',
 
 			FormDataFactory.prototype.initialize = function(ActivitiForm) {
 				this.processDefinitionId = ActivitiForm.processDefinitionId.sProcessDefinitionKeyWithVersion;
+				this.sProcessDefinitionName = ActivitiForm.processDefinitionId.sProcessDefinitionName;
 				for (var key in ActivitiForm.formProperties) {
 					var property = ActivitiForm.formProperties[key];
 					switch (property.type) {
