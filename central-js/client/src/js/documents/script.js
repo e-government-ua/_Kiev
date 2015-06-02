7ffcbd1b1505186bb5c7377c20fa2bfad135fd9c
Dmytro Khalin <d.khalin@gmail.com>
2015-06-02 14:28:00 +0300

    Добавлена загрушка получения доступа к документу

--- central-js/client/src/js/documents/script.js
+++ central-js/client/src/js/documents/script.js
@@ -28,6 +28,18 @@ define('documents', ['angularAMD', 'service'], function(angularAMD) {
           })
         }
       })
+      .state('documents.view', {
+        url: '/view',
+        parent: 'documents',
+        views: {
+          'view': angularAMD.route({
+            templateProvider: function($templateCache) {
+              console.log("Событие срабатывает");
+              return $templateCache.get('html/documents/view.html');
+            }
+          })
+        }       
+      })
       .state('documents.content', {
         url: '/content?code',
         parent: 'documents',
