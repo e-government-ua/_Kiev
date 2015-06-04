4eec62bd95a03f52561f7c29f13ba0e2b5dcfd6a
sybase <bw@pb.ua>
2015-06-04 23:46:43 +0300

    https://github.com/e-government-ua/i/issues/283 and date

--- dashboard-js/server/api/tasks/tasks.controller.js
+++ dashboard-js/server/api/tasks/tasks.controller.js
@@ -13,7 +13,7 @@ exports.index = function(req, res) {
   if (req.query.filterType === 'selfAssigned') {
     query.assignee = user.id;
   } else if (req.query.filterType === 'unassigned') {
-    //query.candidateUser = user.id;
+     query.candidateUser = user.id;
     query.unassigned = true;
   } else if (req.query.filterType === 'finished') {
     query.candidateUser = user.id;
