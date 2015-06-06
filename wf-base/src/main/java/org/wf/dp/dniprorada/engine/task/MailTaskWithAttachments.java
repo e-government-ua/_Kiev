25e3335515fa7ef155bebef0e028cc428a8452f5
Tsapko Dmytro <tsapko.dmitry@gmail.com>
2015-06-07 00:29:06 +0300

    issues-311, fixing MIME type format.

--- wf-base/src/main/java/org/wf/dp/dniprorada/engine/task/MailTaskWithAttachments.java
+++ wf-base/src/main/java/org/wf/dp/dniprorada/engine/task/MailTaskWithAttachments.java
@@ -96,7 +96,7 @@ public class MailTaskWithAttachments implements JavaDelegate {
 
 			for (Attachment attachment : attachmentList) {
 				nameFile = attachment.getName();
-				typeFile = attachment.getType();
+				typeFile = attachment.getType().split(";")[0];
 				System.out.println("typeFile: " + typeFile);
 				description = attachment.getDescription();
 				attachmentStream = execution.getEngineServices().getTaskService()
