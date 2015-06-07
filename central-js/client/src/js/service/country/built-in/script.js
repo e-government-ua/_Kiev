29f148a9a0ed34c3e207979818b141a17027cad4
Artur Kvasnii <artur.kvasnii@gmail.com>
2015-06-07 17:54:17 +0300

    Виправив відображення послуг все Українського рівня.

--- central-js/client/src/js/service/country/built-in/script.js
+++ central-js/client/src/js/service/country/built-in/script.js
@@ -18,7 +18,18 @@ define('service.general.country.built-in', ['angularAMD'], function (angularAMD)
 			.state('service.general.country.built-in.bankid', {
 				url: '/built-in/?code',
 				parent: 'service.general.country',
+				data: {
+					region: null,
+					city: null
+				},
 				resolve: {
+					oService: ['$stateParams', 'service', function($stateParams, service) {
+						return service;
+					}],
+					oServiceData: ['$stateParams', 'service', function($stateParams, service) {
+						var aServiceData = service.aServiceData;
+						return aServiceData[0];
+					}],
 					BankIDLogin: ['$q', '$state', '$location', '$stateParams', 'BankIDService', function($q, $state, $location, $stateParams, BankIDService) {
 						var url = $location.protocol()
 							+'://'
@@ -34,13 +45,30 @@ define('service.general.country.built-in', ['angularAMD'], function (angularAMD)
 					BankIDAccount: ['BankIDService', 'BankIDLogin', function(BankIDService, BankIDLogin) {
 						return BankIDService.account(BankIDLogin.access_token);
 					}],
-					ActivitiForm: ['$stateParams', 'ActivitiService', 'service', 'places', function($stateParams, ActivitiService, service, places) {
-						var aServiceData = service.aServiceData;
-						var oServiceData = null;
-						angular.forEach(aServiceData, function(value, key) {
-							oServiceData = value;
+					processDefinitions: ['ServiceService', 'oServiceData', function(ServiceService, oServiceData) {
+						return ServiceService.getProcessDefinitions(oServiceData, true);
+					}],
+					processDefinitionId: ['oServiceData', 'processDefinitions', function(oServiceData, processDefinitions) {
+						var sProcessDefinitionKeyWithVersion = oServiceData.oData.oParams.processDefinitionId;
+						var sProcessDefinitionKey = sProcessDefinitionKeyWithVersion.split(':')[0];
+						
+						var sProcessDefinitionName = "тест";
+                                                //sProcessDefinitionName = "name2";
+                                                //var currentState = $state.get('service.general.city.built-in.bankid');
+                                                //currentState.data.sProcessDefinitionName2 = "name3";
+                                                
+						angular.forEach(processDefinitions.data, function(value, key) {
+							if(value.key == sProcessDefinitionKey) {
+								sProcessDefinitionKeyWithVersion = value.id;
+                                                                sProcessDefinitionName = "("+value.name+")";
+							}
 						});
-						return ActivitiService.getForm(oServiceData);
+
+						//return processDefinitionKeyWithVersion;
+						return {sProcessDefinitionKeyWithVersion:sProcessDefinitionKeyWithVersion,sProcessDefinitionName:sProcessDefinitionName};
+					}],
+					ActivitiForm: ['ActivitiService', 'oServiceData', 'processDefinitionId', function(ActivitiService, oServiceData, processDefinitionId) {
+						return ActivitiService.getForm(oServiceData, processDefinitionId);
 					}]
 				},
 				views: {
@@ -53,6 +81,24 @@ define('service.general.country.built-in', ['angularAMD'], function (angularAMD)
 					})
 				}
 			})
+			.state('service.general.country.built-in.bankid.submitted', {
+				url: null,
+				data: { id: null },
+				onExit:['$state', function($state) {
+					var state = $state.get('service.general.country.built-in.bankid.submitted');
+					state.data = { id: null };
+				}],
+				views: {
+					'content@service.general.country': angularAMD.route({
+                        templateProvider: ['$templateCache', function($templateCache) {
+							return $templateCache.get('html/service/country/built-in/bankid.submitted.html');
+						}],
+						controller: ['$state', '$scope', function($state, $scope) {
+							$scope.state = $state.get('service.general.country.built-in.bankid.submitted');
+						}]
+					})
+				}
+			});
     }]);
     return app;
 });
