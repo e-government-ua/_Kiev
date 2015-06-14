f855c358c4cae918e70710a1a2b9dd9631a12e5d
Daniil <daniil@redmart.com>
2015-06-14 23:15:22 +0800

    Copy to clibboard for dashboard. Might not work properly for different task types ( I've had chance to test it with one task type only )

--- dashboard-js/server/config/local.env.sample.js
+++ dashboard-js/server/config/local.env.sample.js
@@ -5,21 +5,21 @@
 // You will need to set these on the server you deploy to.
 //
 module.exports = {
-	DEBUG: false,
-	DOMAIN: 'http://localhost:9000',
-	SESSION_SECRET: 'dashboardjs-secret',
+  DEBUG: false,
+  DOMAIN: 'http://localhost:9000',
+  SESSION_SECRET: 'dashboardjs-secret',
 
-	ACTIVITI_PROT: 'https',
-	ACTIVITI_HOST: 'test.igov.org.ua',
-	ACTIVITI_PORT: 8080,
-	ACTIVITI_REST: 'wf-region/service',
-	ACTIVITI_AUTH_BASIC: 'Basic YWN0aXZpdGktbWFzdGVyOlVqaHRKbkV2ZiE=',
-	ACTIVITI_SESSION_IDLE: 3000,
-	ACTIVITI_SESSION_TIMEOUT: 3000,
-	ACTIVITI_SESSION_INTERVAL: 1000,
+  ACTIVITI_PROT: 'https',
+  ACTIVITI_HOST: 'test.igov.org.ua',
+  ACTIVITI_PORT: 8080,
+  ACTIVITI_REST: 'wf-region/service',
+  ACTIVITI_AUTH_BASIC: 'Basic YWN0aXZpdGktbWFzdGVyOlVqaHRKbkV2ZiE=',
+  ACTIVITI_SESSION_IDLE: '3000',
+  ACTIVITI_SESSION_TIMEOUT: '3000',
+  ACTIVITI_SESSION_INTERVAL: '1000',
 
-	PRIVATE_KEY: 'path to [sslcert/server.key]', //works only with SSL_PORT
-	CERTIFICATE: 'path to [sslcert/server.crt]' //works only with SSL_PORT
-	//SSL_PORT: 8084 //ssl port. enable if https is needed for portal
+  PRIVATE_KEY: 'path to [sslcert/server.key]', //works only with SSL_PORT
+  CERTIFICATE: 'path to [sslcert/server.crt]' //works only with SSL_PORT
+  //SSL_PORT: 8084 //ssl port. enalbe if https is needed for portal
 };
 
