013216b5558f219fdfa3205d16df49e321d0dee6
sybase <bw@pb.ua>
2015-06-14 18:42:08 +0300

    -нужно сделать так, чтобы были видны не только кнопки, но и количество шарингов в каждой из сетей. Например, в сервисе, который мы используем, в самом низу такая панель.
    Державним органам:
    додати свою послугу
    
    dubilet [6:08 PM]
    Немає якоїсь
    послуги?
    
    dubilet [6:08 PM]
    Новини порталу
    на Facebook

--- central-js/client/src/js/service/city/controllers/states.js
+++ central-js/client/src/js/service/city/controllers/states.js
@@ -106,6 +106,16 @@ define('state/service/city/absent/controller', ['angularAMD'], function (angular
                     var h=d[g]('body')[0];
                     h.appendChild(s);
                 }})();
+            
+            /*(function() {
+                if (window.pluso)if (typeof window.pluso.start == "function") return;
+                if (window.ifpluso==undefined) { window.ifpluso = 1;
+                  var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                  s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                  s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                  var h=d[g]('body')[0];
+                  h.appendChild(s);
+            }})();*/
 
             // %Населенный пункт% – %Название услуги%
             $scope.absentMessage = {
