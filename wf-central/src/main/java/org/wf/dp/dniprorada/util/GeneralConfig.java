4e20828f6e545711299d7919f12f1725a405f3bb
hudson-206 <hudson-206>
2015-06-15 11:34:17 +0300

    bTest = true;

--- wf-central/src/main/java/org/wf/dp/dniprorada/util/GeneralConfig.java
+++ wf-central/src/main/java/org/wf/dp/dniprorada/util/GeneralConfig.java
@@ -46,7 +46,7 @@ public class GeneralConfig {
             //getProfileProperty("")
             b = (sbTest == null ? b : sbTest.trim().length()>0 ? !"false".equalsIgnoreCase(sbTest.trim()) : true);
             oLog.info("[bTest]:sbTest=" + sbTest);
-            b = false;
+            b = true;
         } catch (Exception oException) {
             oLog.error("[bTest]:sbTest=" + sbTest, oException);
         }
