c8c6e1eff8faa04590fbac73481054de4e8504b4
domash <aleksej.domash@gmail.com>
2015-06-19 01:51:47 +0300

    auth cehcks for other methods

--- central-js/server/api/service/documents.js
+++ central-js/server/api/service/documents.js
@@ -6,7 +6,7 @@ var auth = require('../auth/auth.service');
 router.get('/', auth.isAuthenticated(), documents.index);
 router.get('/:nID', documents.getDocument);
 router.get('/download/:nID', auth.isDocumentOwner(), documents.getDocumentFile);
-router.post('/initialupload', documents.initialUpload);
-router.get('/:nID/share', documents.shareDocument);
+router.post('/initialupload', auth.isAuthenticated(), documents.initialUpload);
+router.get('/:nID/share', auth.isDocumentOwner(), documents.shareDocument);
 
 module.exports = router;
