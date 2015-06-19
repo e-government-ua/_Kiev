6fc928e8e8c13ef5558db8cc087e18c2595808f3
Kiaba <artur.kvasnii@gmail.com>
2015-06-20 02:06:44 +0300

    Removed hard code.

--- central-js/client/src/js/journal/controllers/states.js
+++ central-js/client/src/js/journal/controllers/states.js
@@ -5,7 +5,8 @@
 });*/
 
 define('state/journal/controller', ['angularAMD'], function (angularAMD) {
-	angularAMD.controller('JournalController', ['$rootScope', '$state', '$window', '$location', function ($rootScope, $state, $window, $location) {
+	angularAMD.controller('JournalController', ['$scope', '$state', 'config', function ($scope, $state, config) {
+		$scope.config = config;
 		if ($state.is('journal')) {
 			return $state.go('journal.bankid');
 		}
@@ -19,7 +20,7 @@ define('state/journal/bankid/controller', ['angularAMD'], function (angularAMD) 
             var stateForRedirect = $state.href('journal.bankid', {});
             var redirectURI = $location.protocol() + '://' + $location.host() + ':' + $location.port() + stateForRedirect;
             //$window.location.href = 'https://bankid.org.ua/DataAccessService/das/authorize?response_type=code&client_id=9b0e5c63-9fcb-4b11-84ff-31fc2cea8801&redirect_uri=' + redirectURI;
-            $window.location.href = 'https://bankid.privatbank.ua/DataAccessService/das/authorize?response_type=code&client_id=9b0e5c63-9fcb-4b11-84ff-31fc2cea8801&redirect_uri=' + redirectURI;
+			$window.location.href = $scope.config.sProtocol_AccessService_BankID + '://' + $scope.config.sHost_AccessService_BankID + '/DataAccessService/das/authorize?response_type=code&client_id=' + $scope.config.client_id + '&redirect_uri=' + redirectURI;
         }
 		
         if ($state.is('journal.bankid') && !!$state.params.code) {
