5fbb6bd85c6baf36596bb57f81886156f54eeea1
Daniil <daniil@redmart.com>
2015-06-26 20:50:28 +0800

    Доработка формы печати заявления на дашборде #434

--- dashboard-js/server/api/process/process.controller.js
+++ dashboard-js/server/api/process/process.controller.js
@@ -4,20 +4,20 @@ var activiti = require('../../components/activiti');
 
 // Get list of processs
 exports.index = function(req, res) {
-    
-        var query = {};
-        query.size = 500;
-    
-	var options = {
-		path: 'repository/process-definitions'
-                , query: query
-	};
 
-	activiti.get(options, function(error, statusCode, result) {
-		if (error) {
-			res.send(error);
-		} else {
-			res.json(result);
-		}
-	});
+  var query = {};
+  query.size = 500;
+
+  var options = {
+    path: 'repository/process-definitions',
+    query: query
+  };
+
+  activiti.get(options, function(error, statusCode, result) {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(result);
+    }
+  });
 };
