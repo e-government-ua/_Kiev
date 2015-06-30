a51b937ae075bef71320b3a1fb1a0dc2e26b7902
domash <aleksej.domash@gmail.com>
2015-07-01 00:37:24 +0300

    refactoring promises in initial uploading, adding error showing logic

--- central-js/client/src/js/documents/controllers/states.js
+++ central-js/client/src/js/documents/controllers/states.js
@@ -24,7 +24,7 @@ define('state/documents/bankid/controller', ['angularAMD'], function (angularAMD
         $scope.error = undefined;
 
         $scope.loginWithBankId = function () {
-            var stateForRedirect = $state.href('documents.bankid', {});
+            var stateForRedirect = $state.href('documents.bankid', {error: ''});
             var redirectURI = $location.protocol() +
                 '://' + $location.host() + ':'
                 + $location.port()
@@ -33,13 +33,21 @@ define('state/documents/bankid/controller', ['angularAMD'], function (angularAMD
         };
 
         if ($state.is('documents.bankid')) {
-            if($state.params.error){
-                $scope.error = JSON.parse($state.params.error).error;
-            } else {
+            if (!$state.params.error) {
                 BankIDService.isLoggedIn().then(function () {
                     $scope.authProcess = true;
-                    return $state.go('documents.content', {code: $state.params.code});
+                    return $state.go('documents.content').catch(function (fallback) {
+                        $state.go('documents.bankid', {error: fallback.error});
+                    }).finally(function () {
+                        $scope.authProcess = false;
+                    });
                 });
+            } else {
+                try {
+                    $scope.error = JSON.parse($state.params.error).error;
+                } catch (error) {
+                    $scope.error = $state.params.error;
+                }
             }
         }
     });
