22bc754c95554867ba1e2744bc283fe4f7bf643e
olya <olya@olya-comp>
2015-07-01 17:19:42 +0300

    add servise getDocumentTypes

---
+++ wf-central/src/main/java/org/wf/dp/dniprorada/dao/DocumentTypeDaoImpl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.wf.dp.dniprorada.dao;
+
+/**
+ *
+ * @author olya
+ */
+public class DocumentTypeDaoImpl {
+    
+}
