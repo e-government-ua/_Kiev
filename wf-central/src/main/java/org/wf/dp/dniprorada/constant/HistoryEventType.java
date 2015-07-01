f0d302b969340d3e4c023e3140751cabce0bbc4b
LamronNu <olga.prylypko@gmail.com>
2015-07-01 21:23:36 +0300

    issue #309, update messages. events -- the same

--- wf-central/src/main/java/org/wf/dp/dniprorada/constant/HistoryEventType.java
+++ wf-central/src/main/java/org/wf/dp/dniprorada/constant/HistoryEventType.java
@@ -9,27 +9,35 @@ public enum HistoryEventType {
     GET_SERVICE(1L,
             "getService",
             "Пользователь воспользовался услугой на портале",
-            "Ви подали заявку на послугу %Назва послуги%. \n Cтатус: %статус%" ),
+            "Ви подали заявку на послугу " + HistoryEventMessage.SERVICE_NAME
+                    + ". \n Cтатус: " + HistoryEventMessage.SERVICE_STATE),
     SET_DOCUMENT_INTERNAL(2L,
             "setDocument_internal",
             "В Мои документы пользователя загружен новый документ – через наш портал",
-            "%Назва органу% завантажує %Тип документу% %Назва документу% у Ваш розділ Мої документи"),
+            HistoryEventMessage.ORGANIZATION_NAME + " завантажує " + HistoryEventMessage.DOCUMENT_TYPE
+                    + " " + HistoryEventMessage.DOCUMENT_NAME + " у Ваш розділ Мої документи"),
     SET_DOCUMENT_EXTERNAL(3L,
             "setDocument_external",
             "В Мои документы пользователя загружен новый документ – внешняя организация",
-            "%Назва органу% завантажує %Тип документу% %Назва документу% у Ваш розділ Мої документи"),
+            HistoryEventMessage.ORGANIZATION_NAME + " завантажує " + HistoryEventMessage.DOCUMENT_TYPE
+                    + " " + HistoryEventMessage.DOCUMENT_NAME + " у Ваш розділ Мої документи"),
     SET_DOCUMENT_ACCESS_LINK(4L,
             "setDocumentAccessLink",
             "Пользователь предоставил доступ к своему документу",
-            "Ви надаєте доступ до документу %Тип документу% %Назва документу% іншій людині: %ФИОІм’я того, кому надають доступ% (телефон: %телефон%)"),
+            "Ви надаєте доступ до документу "
+                    + HistoryEventMessage.DOCUMENT_TYPE + " " + HistoryEventMessage.DOCUMENT_NAME
+                    + " іншій людині: " + HistoryEventMessage.FIO
+                    + " (телефон: " + HistoryEventMessage.TELEPHONE + ")"),
     SET_DOCUMENT_ACCESS(5L,
             "setDocumentAccess",
             "Кто-то воспользовался доступом к документу через OTP, который ему предоставил пользователь",
-            "%Ім’я того, кому надають доступ% скористався доступом, який Ви надали, та переглянув документ %Тип документу% %Назва документу%"),
+            "" + HistoryEventMessage.SERVICE_NAME + " скористався доступом, який Ви надали, та переглянув документ "
+                    + HistoryEventMessage.SERVICE_NAME + " " + HistoryEventMessage.SERVICE_NAME + ""),
     ACTIVITY_STATUS_NEW(6L,
             "ActivitiStatusNew",
             "Изменение статуса заявки",
-            "Ваша заявка №%nTask% изменила свой статус на %StatusNameNew%");
+            "Ваша заявка №" + HistoryEventMessage.TASK_NUMBER
+                    + " изменила свой статус на " + HistoryEventMessage.SERVICE_STATE + "");
 
     private Long nID;
     private String sID;
