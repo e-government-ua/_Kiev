f7bd53a5ef6513da8d5829ccecf5f2a92d122e59
LamronNu <olga.prylypko@gmail.com>
2015-07-07 20:23:51 +0300

    #493, without algorithm Luna

---
+++ wf-central/src/main/java/org/wf/dp/dniprorada/model/HistoryEvent_Service.java
@@ -0,0 +1,64 @@
+package org.wf.dp.dniprorada.model;
+
+import org.wf.dp.dniprorada.base.model.Entity;
+
+import javax.persistence.Column;
+
+@javax.persistence.Entity
+public class HistoryEvent_Service extends Entity {
+
+    @Column
+    private String sID;
+
+    @Column
+    private Long nID_Task;
+
+    @Column
+    private Long nID_Subject;
+
+    @Column
+    private String sStatus;
+
+    @Column
+    private String sID_Status;
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+
+    public Long getnID_Task() {
+        return nID_Task;
+    }
+
+    public void setnID_Task(Long nID_Task) {
+        this.nID_Task = nID_Task;
+    }
+
+    public Long getnID_Subject() {
+        return nID_Subject;
+    }
+
+    public void setnID_Subject(Long nID_Subject) {
+        this.nID_Subject = nID_Subject;
+    }
+
+    public String getsStatus() {
+        return sStatus;
+    }
+
+    public void setsStatus(String sStatus) {
+        this.sStatus = sStatus;
+    }
+
+    public String getsID_Status() {
+        return sID_Status;
+    }
+
+    public void setsID_Status(String sID_Status) {
+        this.sID_Status = sID_Status;
+    }
+}
