dcdddee85b028cae4bfe0a005f8504703cf800ee
dgroup <ydantezs@yandex.ru>
2015-07-08 02:08:10 +0300

    https://github.com/e-government-ua/i/issues/487 started: [Implemented] getSubjectOrganJoins [In progress] setSubjectOrganJoins [Planned] removeSubjectOrganJoins

--- wf-central/src/test/java/org/activiti/rest/controller/ActivitiRestDocumentControllerTest.java
+++ wf-central/src/test/java/org/activiti/rest/controller/ActivitiRestDocumentControllerTest.java
@@ -3,6 +3,7 @@ package org.activiti.rest.controller;
 import org.activiti.rest.controller.util.ErrMessage;
 import org.apache.commons.collections.Predicate;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +17,19 @@ import org.springframework.web.context.WebApplicationContext;
 import org.wf.dp.dniprorada.base.util.JsonRestUtils;
 import org.wf.dp.dniprorada.model.Document;
 import org.wf.dp.dniprorada.model.DocumentOperator_SubjectOrgan;
+import org.wf.dp.dniprorada.model.SubjectOrganJoin;
 
-import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.apache.commons.collections.CollectionUtils.find;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.wf.dp.dniprorada.model.DocumentOperatorTest.DUMMY_OPERATOR_ID;
+
 
 /**
  * @author dgroup
@@ -57,7 +61,7 @@ public class ActivitiRestDocumentControllerTest {
             .getResponse()
             .getContentAsString();
 
-        List<DocumentOperator_SubjectOrgan> operators = Arrays.asList(JsonRestUtils.readObject(jsonData, DocumentOperator_SubjectOrgan[].class));
+        List<DocumentOperator_SubjectOrgan> operators = asList(JsonRestUtils.readObject(jsonData, DocumentOperator_SubjectOrgan[].class));
 
         assertFalse(operators.isEmpty());
 
@@ -212,5 +216,91 @@ public class ActivitiRestDocumentControllerTest {
         ErrMessage msg = JsonRestUtils.readObject(jsonData, ErrMessage.class);
         assertNotNull("Expected error message not found", msg);
         assertEquals("Document Access not found", msg.getMessage());
+    }
+
+
+    @Test
+    public void getSubjectOrganJoinByOrganID() throws Exception {
+        final Long region = 11L;
+        final Long city   = 33L;
+
+        String jsonData = mockMvc
+            .perform(get("/services/getSubjectOrganJoins")
+                    .param("nID_SubjectOrgan", "1")
+                    .param("nID_Region", "11")
+                    .param("nID_City", "33"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        List<SubjectOrganJoin> operators = asList(
+                JsonRestUtils.readObject(jsonData, SubjectOrganJoin[].class));
+
+        assertFalse("Two or three test operators must be here", operators.isEmpty());
+
+        SubjectOrganJoin mvsGovUa = (SubjectOrganJoin)
+            find(operators, new Predicate() {
+                public boolean evaluate(Object object) {
+                    SubjectOrganJoin mvs = (SubjectOrganJoin) object;
+                    return region.equals(mvs.getRegionId()) &&
+                            city.equals(mvs.getCityId());
+                }
+            });
+
+        assertEquals("ID aren't match", 356L, mvsGovUa.getId().longValue()); // compile error: Long vs Object
+
+        jsonData = mockMvc
+            .perform(get("/services/getSubjectOrganJoins")
+                    .param("nID_SubjectOrgan", "2"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        operators = asList(JsonRestUtils.readObject(jsonData, SubjectOrganJoin[].class));
+        assertFalse("Only one row expected, test data error", operators.size() > 1);
+
+        mvsGovUa = operators.iterator().next();
+        assertEquals("ID[2] aren't match", 358L, mvsGovUa.getId().longValue()); // compile error: Long vs Object
+    }
+
+    @Test @Ignore
+    public void setSubjectOrganJoins() throws Exception {
+        SubjectOrganJoin soj = new SubjectOrganJoin();
+        soj.setId(400L);
+        soj.setSubjectOrganId(5L);
+        soj.setNameUa("Українська мова");
+        soj.setNameRu("Русский язык");
+        soj.setPrivatId("12345");
+        soj.setPublicId("55555");
+        soj.setGeoLongitude("15.232312");
+        soj.setGeoLatitude("23.234231");
+
+        String jsonSoj = JsonRestUtils.toJson(soj);
+
+        mockMvc.perform(post("/services/setSubjectOrganJoins")
+                .param("subjectOrganJoin", jsonSoj));
+
+        jsonSoj = mockMvc.
+            perform(get("/services/getSubjectOrganJoins")
+                    .param("nID_SubjectOrgan", "5"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        List<SubjectOrganJoin> sojs =asList(JsonRestUtils.readObject(jsonSoj, SubjectOrganJoin[].class));
+        assertTrue("Only one element should be available", sojs.size() == 1);
+
+        SubjectOrganJoin persistedSoj = sojs.iterator().next();
+
+        assertEquals("ID's aren't match", soj.getId(), persistedSoj.getId());
+        assertEquals("Public ID's aren't match", soj.getPublicId(), persistedSoj.getPublicId());
+        assertEquals("Longitude aren't match", soj.getGeoLongitude(), persistedSoj.getGeoLongitude());
+        assertEquals("Latitude aren't match", soj.getGeoLatitude(), persistedSoj.getGeoLatitude());
     }
 }
