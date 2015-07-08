6cb05b1e9b607d7a19250b66f0e6941ea79ad1a7
olya <olya@10.43.15.154>
2015-07-08 11:33:09 +0300

    chage return type of removeAccessData

--- wf-base/src/main/java/org/wf/dp/dniprorada/base/dao/AccessDataDao.java
+++ wf-base/src/main/java/org/wf/dp/dniprorada/base/dao/AccessDataDao.java
@@ -5,5 +5,5 @@ public interface AccessDataDao {
     public String setAccessData(String content);
     public String setAccessData(byte[] content);
     public String getAccessData(String sKey);
-    public void removeAccessData(String sKey);
+    public boolean removeAccessData(String sKey);
 }
