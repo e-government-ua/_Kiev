104464e86c6f80d5e94ebaff5798987934093e04
Daniil <daniil@redmart.com>
2015-07-10 01:30:50 +0800

    Centraljs. Fix ci issue. SVGmin is removed

--- central-js/Gruntfile.js
+++ central-js/Gruntfile.js
@@ -284,18 +284,6 @@ module.exports = function(grunt) {
         }]
       }
     },
-
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.svg',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
     ngAnnotate: {
@@ -424,8 +412,7 @@ module.exports = function(grunt) {
       },
       dist: [
         'sass',
-        'imagemin',
-        'svgmin'
+        'imagemin'
       ]
     },
 
