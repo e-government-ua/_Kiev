69f193fffb5795913a2d318d7a668b2b3ad657b0
yevheniilazarev <evgenij.lazarev@privatbank.ua>
2015-07-09 16:33:02 +0300

    #issue512 create service setPaymentStatus_TaskActiviti

--- wf-base/src/main/java/org/activiti/rest/controller/ActivitiPaymentRestController.java
+++ wf-base/src/main/java/org/activiti/rest/controller/ActivitiPaymentRestController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/setPaymentStatus_TaskActiviti", method = RequestMethod.GET)
 public class ActivitiPaymentRestController {
+	@RequestMapping(value = "/setPaymentStatus_TaskActiviti", method = RequestMethod.GET)
 	public @ResponseBody String setPaymentStatus_TaskActiviti(
 			@RequestParam String sID_Order,
 			@RequestParam String sData,
