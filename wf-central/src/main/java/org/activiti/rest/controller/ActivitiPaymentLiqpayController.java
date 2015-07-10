e4596e745fe7056b86a3a16ac86586fb789304f7
yevheniilazarev <evgenij.lazarev@privatbank.ua>
2015-07-10 14:15:37 +0300

    #issue512 create service setPaymentNewStatus_Liqpay

--- wf-central/src/main/java/org/activiti/rest/controller/ActivitiPaymentLiqpayController.java
+++ wf-central/src/main/java/org/activiti/rest/controller/ActivitiPaymentLiqpayController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.wf.dp.dniprorada.util.Util;
 
 @Controller
@@ -14,8 +15,8 @@ public class ActivitiPaymentLiqpayController {
 	private final Logger log = LoggerFactory.getLogger(ActivitiPaymentLiqpayController.class);
 	private StringBuffer sb = new StringBuffer();
 	
-	@RequestMapping(value="/setPaymentNewStatus_Liqpay", method = RequestMethod.GET)
-	public String setPaymentNewStatus_Liqpay(
+	@RequestMapping(value="/setPaymentNewStatus_Liqpay", method = RequestMethod.GET, headers = {"Accept=application/json"})
+	public @ResponseBody String setPaymentNewStatus_Liqpay(
 			@RequestParam String sID_Order,
 			@RequestParam String sHost) {
 		sb.append(sHost);
