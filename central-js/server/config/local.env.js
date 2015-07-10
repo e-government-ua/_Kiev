2352458d01d2280bb2a58e8022970cf5c7f3ac5a
sybase <bw@pb.ua>
2015-07-11 01:19:27 +0300

    shel scripts fix+

---
+++ central-js/server/config/local.env.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Use local.env.js for environment variables that grunt will set when the server starts locally.
+// Use for your api keys, secrets, etc. This file should not be tracked by git.
+//
+// You will need to set these on the server you deploy to.
+
+module.exports = {
+  DOMAIN: 'http://localhost:9000',
+  // Control debug level for modules using visionmedia/debug
+  DEBUG: 'true',
+
+  SERVER_PROTOCOL: 'http',
+  SERVER_PORT: '9000',
+  SERVER_KEY: '/sybase/cert/server.key',
+  SERVER_CERT: '/sybase/cert/server.crt',
+
+  SESSION_SECRET: 'put yor session secret here',
+  SESSION_KEY_ONE: 'solt1 for session',
+  SESSION_KEY_TWO: 'solt2 for session',
+  SESSION_SECURE: true,
+  SESSION_MAX_AGE: 180000, // 3 * 60 * 1000 = 3 min
+
+  ACTIVITI_PROTOCOL: 'https',
+  ACTIVITI_HOSTNAME: 'test.igov.org.ua',
+  ACTIVITI_PORT: '8443',
+  ACTIVITI_PATH: '/wf-central/service',
+  ACTIVITI_USER: 'activiti-master',
+  ACTIVITI_PASSWORD: 'UjhtJnEvf!',
+
+  BANKID_SPROTOCOL_ACCESS_SERVICE: 'https',
+  BANKID_SHOST_ACCESS_SERVICE: 'bankid.privatbank.ua',
+  BANKID_SPROTOCOL_RESOURC_SERVICE: 'https',
+  BANKID_SHOST_RESOURCE_SERVICE: 'bankid.privatbank.ua',
+  BANKID_CLIENTID: 'testIgov',
+  BANKID_CLIENT_SECRET: 'testIgovSecret'
+};
