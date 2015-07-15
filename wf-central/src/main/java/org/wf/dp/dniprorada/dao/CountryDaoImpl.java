e97ccc317f80a6e2cf5629d1b0315c361682792c
LamronNu <olga.prylypko@gmail.com>
2015-07-15 23:03:41 +0300

    #515, all structure and services get-set-remove (without data)

---
+++ wf-central/src/main/java/org/wf/dp/dniprorada/dao/CountryDaoImpl.java
@@ -0,0 +1,138 @@
+package org.wf.dp.dniprorada.dao;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+import org.wf.dp.dniprorada.base.dao.AbstractEntityDao;
+import org.wf.dp.dniprorada.model.Country;
+import org.wf.dp.dniprorada.model.EntityNotFoundException;
+
+import java.util.List;
+
+public class CountryDaoImpl extends AbstractEntityDao<Country>
+        implements CountryDao{
+
+    private static final Logger log = Logger.getLogger(CountryDaoImpl.class);
+
+    protected CountryDaoImpl() {
+        super(Country.class);
+    }
+
+    //return false if all args are null
+    //otherwise set params to criteria
+    //note: length arrays must be the same
+    private boolean setRestrictions(Criteria criteria, String[] columns, Object[] values) {
+        boolean allAreNull = true;
+        for (int i = 0; i < values.length; i++) {
+            Object value = values[i];
+            if (value != null){
+                criteria.add(Restrictions.eq(columns[i], value));
+                allAreNull = false;
+            }
+        }
+        return !allAreNull;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override //отдает массив объектов сущности, подпадающих под критерии
+    // later edit or delete this method
+    public List<Country> getAll(Long nID_ua, String sID_two, String sID_three,
+                                String sNameShort_ua, String sNameShort_en) {
+
+        Criteria criteria = getSession().createCriteria(Country.class);
+        boolean setRestrictions = setRestrictions(criteria,
+                new String[]{"nID_UA", "sID_Two", "sID_Three", "sNameShort_UA", "sNameShort_EN"},
+                new Object[]{nID_ua, sID_two, sID_three, sNameShort_ua, sNameShort_en});
+
+        if (!setRestrictions)
+            throw new IllegalArgumentException("all args are null!");
+
+        return (List<Country>) criteria.list();
+    }
+
+    @Override   //апдейтит элемент(если задан один из уникальных-ключей)
+                //или вставляет (если не задан nID), и отдает экземпляр нового объекта
+    public Country setCountry(Long nID, Long nID_ua, String sID_two, String sID_three,
+                              String sNameShort_ua, String sNameShort_en, String sReference_localISO) {
+
+        Country country = getByKey(nID, nID_ua, sID_two, sID_three);
+        //check nID
+//        if (nID != null){//update
+//            country = getById(nID);
+//            if (country == null)
+//                throw new EntityNotFoundException("Record not found!");
+//        } else {        //insert
+//
+//                    new Country();
+//        }
+        if (country == null){
+            if (nID == null) {
+                country = new Country();
+            } else {
+                throw new EntityNotFoundException("Record not found!");
+            }
+        }
+        //set values (if they are not null and not the same
+        if (nID_ua != null && country.getnID_UA() != nID_ua)
+            country.setnID_UA(nID_ua);
+        if (sID_two != null && !sID_two.equals(country.getsID_Two()))
+            country.setsID_Two(sID_two);
+        if (sID_three != null && !sID_three.equals(country.getsID_Three()))
+            country.setsID_Three(sID_three);
+        if (sNameShort_ua != null && !sNameShort_ua.equals(country.getsNameShort_UA()))
+            country.setsNameShort_UA(sNameShort_ua);
+        if (sNameShort_en != null && !sNameShort_en.equals(country.getsNameShort_EN()))
+            country.setsNameShort_EN(sNameShort_en);
+        if (sReference_localISO != null && !sReference_localISO.equals(country.getsReference_LocalISO()))
+            country.setsReference_LocalISO(sReference_localISO);
+
+        saveOrUpdate(country);
+
+        return country;
+    }
+
+    //return true if all args are null
+    private boolean areAllArgsNull(Object... args) {
+        boolean result = true;
+        for (Object o : args) {
+            if (o != null){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override //удаляет элемент(по одному из уникальных-ключей)
+    public void removeByKey(Long nID, Long nID_ua, String sID_two, String sID_three) {
+         Country country = getByKey(nID, nID_ua, sID_two, sID_three);
+        if (country == null){
+            throw new EntityNotFoundException("Record not found!");
+        } else {
+            delete(country);
+        }
+    }
+
+    @Override
+    public Country getByKey(Long nID, Long nID_ua, String sID_two, String sID_three) {
+        if (nID != null){
+            return getById(nID);
+        } else
+        if (nID_ua != null){
+            return getRecordByField("nID_UA", nID_ua);
+        } else
+        if (sID_two != null){
+            return getRecordByField("sID_Two", sID_two);
+        } else
+        if (sID_three != null){
+            return getRecordByField("sID_Three", sID_three);
+        } else
+            throw new IllegalArgumentException("All args are null!");
+    }
+
+    private Country getRecordByField(String keyName, Object keyValue) {
+        Criteria cr = getSession().createCriteria(Country.class);
+        cr.add(Restrictions.eq(keyName, keyValue));
+        return (Country) cr.uniqueResult();
+    }
+}
