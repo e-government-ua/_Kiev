b79e13d90a0ac711a9f2d723b93457ae8b97d40d
sybase <bw@pb.ua>
2015-07-19 19:11:13 +0300

    fix AccessData + fix convertation byte vs string

--- wf-base/src/main/java/org/wf/dp/dniprorada/base/dao/AccessDataDaoImpl.java
+++ wf-base/src/main/java/org/wf/dp/dniprorada/base/dao/AccessDataDaoImpl.java
@@ -1,5 +1,7 @@
 package org.wf.dp.dniprorada.base.dao;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import ua.org.egov.utils.storage.durable.impl.GridFSBytesDataStorage;
@@ -10,7 +12,7 @@ import org.slf4j.LoggerFactory;
 public class AccessDataDaoImpl implements AccessDataDao {
 
     private static final String contentMock = "No content!!!";
-    private final Logger log = LoggerFactory.getLogger(AccessDataDaoImpl.class);
+    private final static Logger log = LoggerFactory.getLogger(AccessDataDaoImpl.class);
 
     @Autowired
     private GridFSBytesDataStorage durableBytesDataStorage;
@@ -19,7 +21,8 @@ public class AccessDataDaoImpl implements AccessDataDao {
     public String setAccessData(String sContent) {
         log.info("[setAccessData]:sContent="+sContent);
         //String sKey=durableBytesDataStorage.saveData(Util.contentStringToByte(sContent));
-        String sKey=durableBytesDataStorage.saveData(sContent.getBytes());
+        //String sKey=durableBytesDataStorage.saveData(sContent.getBytes());
+        String sKey=durableBytesDataStorage.saveData(aByte(sContent));
         log.info("[setAccessData]:sKey="+sKey);
         log.info("[setAccessData]:sData(check)="+getAccessData(sKey));
         return sKey;
@@ -28,11 +31,32 @@ public class AccessDataDaoImpl implements AccessDataDao {
     @Override
     public String setAccessData(byte[] aContent) {
         //log.info("[setAccessData]:sContent="+(aContent==null?"null":Util.contentByteToString(aContent)));
-        log.info("[setAccessData]:sContent="+(aContent==null?"null":Arrays.toString(aContent)));
+        log.info("[setAccessData]:sContent="+(aContent==null?"null":Arrays.toString(aContent))+",sByte(aContent)="+sByte(aContent));
         String sKey=durableBytesDataStorage.saveData(aContent);
         log.info("[setAccessData]:sKey="+sKey);
         return sKey;
     }
+    
+    public static String sByte(byte[] a){
+        //Charset.forName("UTF-8")
+        //byte[] b = {(byte) 99, (byte)97, (byte)116};
+        String s = "Not convertable!";
+        log.info("[sByte]:a.length="+a.length+",Arrays.toString(a)="+Arrays.toString(a));
+        try{
+            s = new String(a, "UTF-8");
+        }catch(Exception oException){
+            log.error("[sByte]",oException);
+        }
+        log.info("[sByte]:s="+s);
+        return s;
+    }
+    public static byte[] aByte(String s){
+        log.info("[aByte]:s="+s);
+        byte[] a = s.getBytes(Charset.forName("UTF-8"));
+        log.info("[aByte]:a.length="+a.length+",Arrays.toString(a)="+Arrays.toString(a));
+        return a;
+    }
+    
 
     @Override
     public String getAccessData(String sKey) {
@@ -42,7 +66,8 @@ public class AccessDataDaoImpl implements AccessDataDao {
         if(aContent != null){
             log.info("[getAccessData]:sKey="+sKey+",aContent.length()="+aContent.length);
             //sData = Util.contentByteToString(aContent);
-            sData = Arrays.toString(aContent);
+            //sData = Arrays.toString(aContent);
+            sData = sByte(aContent);
             //log.info("[getAccessData]:TEST:sKey="+sKey+",Arrays.toString(aContent)="+Arrays.toString(aContent));
             if(sData!=null){
                 log.info("[getAccessData]:sKey="+sKey+",sData.length()="+sData.length());
