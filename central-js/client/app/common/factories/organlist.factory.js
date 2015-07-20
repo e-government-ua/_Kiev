b13e427e6b5bdac8ef3d184cc630cc07d21443bd
hordesalik <hordesalik@gmail.com>
2015-07-20 21:37:10 +0300

    #518 на главном портале в форме подвязать к типу "select", и id=sID_Public_SubjectOrganJoin сответствующую сущность SubjectOrganJoin

--- central-js/client/app/common/factories/organlist.factory.js
+++ central-js/client/app/common/factories/organlist.factory.js
@@ -28,7 +28,7 @@ angular.module('app').factory('OrganListFactory', function($http, $filter, Typea
       nID_Region: oServiceData.nID_City && oServiceData.nID_City.nID_Region ? oServiceData.nID_City.nID_Region.nID : null,
       sID_UA: sID_UA
     };
-    return this.typeahead.load('./api/organs/' + oServiceData.nID, search, data).then(function(organs) {
+    return this.typeahead.load('./api/organs/' + oServiceData.oSubject_Operator.oSubject.nID, search, data).then(function(organs) {
       if (search && search.length > 0 && search !== '[$empty$]')
         return $filter('filter')(organs, {sNameUa:search});
       else
