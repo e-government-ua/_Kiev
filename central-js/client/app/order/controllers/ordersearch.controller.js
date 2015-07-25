86298ec12eb987d7a89f825dfb80c9430be1eb32
Oleksii Golub <ogolub@speroteck.com>
2015-07-25 15:57:51 +0300

    issue #505 - fix

--- central-js/client/app/order/controllers/ordersearch.controller.js
+++ central-js/client/app/order/controllers/ordersearch.controller.js
@@ -11,6 +11,7 @@ angular.module('order').controller('OrderSearchController', function($rootScope,
                     $scope.messages = ['Завявка не знайдена'];
                 } else {
                     if (typeof data === 'object') {
+                        data.sDateEdit = new Date();
                         data = [data];
                     }
                     $scope.orders = data;
