d9cd9365bd9feaf6a854b1a7ef8f4520de4299bc
dgroup <ydantezs@yandex.ru>
2015-07-27 23:20:34 +0300

    https://github.com/e-government-ua/i/issues/573 - Implementation started;

---
+++ wf-central/src/main/java/org/wf/dp/dniprorada/dao/PlaceDaoImpl.java
@@ -0,0 +1,62 @@
+package org.wf.dp.dniprorada.dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.wf.dp.dniprorada.base.dao.AbstractEntityDao;
+import org.wf.dp.dniprorada.base.dao.BaseEntityDao;
+import org.wf.dp.dniprorada.model.Place;
+import org.wf.dp.dniprorada.model.PlaceTree;
+import org.wf.dp.dniprorada.model.PlaceType;
+import org.wf.dp.dniprorada.util.Tree;
+
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+/**
+ * @author dgroup
+ * @since  20.07.2015
+ */
+public class PlaceDaoImpl implements PlaceDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    private BaseEntityDao baseEntityDao;
+
+    public PlaceDaoImpl(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public List<Place> findBy(Long placeId, String uaId, Boolean tree) {
+        Criteria places = sessionFactory
+            .getCurrentSession()
+            .createCriteria(Place.class);
+
+        if(isNotBlank(uaId))
+            places = places.add( Restrictions.eq("uaId", uaId) );
+
+        return places.list();
+    }
+
+    @SuppressWarnings("unchecked")
+    public Tree<Place> findPlacesTreeBy(Long placeId, String uaId, Long typeId, Boolean area, Boolean root, Integer deep) {
+        List<Object[]> results = sessionFactory
+                .getCurrentSession()
+                .createSQLQuery("")
+                .list();
+
+        return null;
+    }
+
+    private boolean specified(Long value) {
+        return value != null && value > 0;
+    }
+
+
+}
