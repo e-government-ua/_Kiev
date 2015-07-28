57a869aa257f3ec39c1dad9ca944ae2a8e428e48
Rost Siryk <rostyslav.siryk+github@globallogic.com>
2015-07-29 00:58:07 +0300

    integrate intlTelInput into angular (wip-2)

--- central-js/client/app/common/components/form/directives/intlTelInput.js
+++ central-js/client/app/common/components/form/directives/intlTelInput.js
@@ -1,40 +1,47 @@
 angular.module('app').directive('intlTelInput', function() {
-	
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            angular.element('input[type=tel]').intlTelInput({
-		        defaultCountry: 'auto',
-		        autoFormat: true,
-		        allowExtensions: false,
-		        preferredCountries: ['ua'],
-		        autoPlaceholder: false,
-		        geoIpLookup: function(callback) {
-		            $.get('http://ipinfo.io', function() {}, 'jsonp').always(function(resp) {
-		                var countryCode = (resp && resp.country) ? resp.country : '';
-		                callback(countryCode);
-		            });
-		        }
-		    }).(scope.$eval(attrs.directiveName));
-    };
-}); 
+	console.log( 'this: ', this );
+	console.log( 'this: ', this );
 
+	function link( scope, element, attrs ) {
+		//$(element).'pluginActivationFunction'(scope.$eval(attrs.directiveName));
+		console.log( 'scope: ', scope );
+		console.log( 'element: ', element );
+		console.log( 'attrs: ', attrs );
+		//$('input[type=tel]').intlTelInput(scope.$eval(attrs.intlTelInput));
+		// {
+	 //        defaultCountry: 'auto',
+	 //        autoFormat: true,
+	 //        allowExtensions: false,
+	 //        preferredCountries: ['ua'],
+	 //        autoPlaceholder: false,
+	 //        geoIpLookup: function(callback) {
+	 //            $.get('http://ipinfo.io', function() {}, 'jsonp').always(function(resp) {
+	 //                var countryCode = (resp && resp.country) ? resp.country : '';
+	 //                callback(countryCode);
+	 //            });
+	 //        }
+	 //    });
+	};
 
+	return {
+    	template: 'Name: '
+  	};
 
-/*
-
-	$('input[type=tel]').intlTelInput({
-          defaultCountry: 'auto',
-          autoFormat: true,
-          allowExtensions: false,
-          preferredCountries: ['ua'],
-          autoPlaceholder: false,
-          geoIpLookup: function(callback) {
-              $.get('http://ipinfo.io', function() {}, 'jsonp').always(function(resp) {
-                  var countryCode = (resp && resp.country) ? resp.country : '';
-                  callback(countryCode);
-              });
-          }
-      });
-
-*/
+/* $('input[type=tel]').intlTelInput({
+      defaultCountry: 'auto',
+      autoFormat: true,
+      allowExtensions: false,
+      preferredCountries: ['ua'],
+      autoPlaceholder: false,
+      geoIpLookup: function(callback) {
+          $.get('http://ipinfo.io', function() {}, 'jsonp').always(function(resp) {
+              var countryCode = (resp && resp.country) ? resp.country : '';
+              callback(countryCode);
+          });
+      }
+  }); */
+    // return {
+    //     restrict: 'A',
+    //     link: link
+    // };	
+});
