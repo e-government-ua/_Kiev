8652743e65dc4722e96e1363839a99133ff1d78c
olya <olya@olya-comp>
2015-08-08 18:16:57 +0300

    https://github.com/e-government-ua/i/issues/655

--- wf-base/src/main/java/org/activiti/rest/interceptor/RequestProcessingInterceptor.java
+++ wf-base/src/main/java/org/activiti/rest/interceptor/RequestProcessingInterceptor.java
@@ -139,7 +139,7 @@ public class RequestProcessingInterceptor extends HandlerInterceptorAdapter {
                     taskName = "Заявка подана";
                     HistoricProcessInstance historicProcessInstance = 
                             historyService.createHistoricProcessInstanceQuery().processInstanceId(sID_Proccess).singleResult();
-                    params.put("sProcessInstanceName", historicProcessInstance.getName());
+                    params.put("sProcessInstanceName", historicProcessInstance.getName() != null ? sProcessInstanceName : "Non name!");
                 } else if (closeTask) {
                     //sID_Proccess = (String) jsonObjectRequest.get("id");
                     serviceName = "updateHistoryEvent_Service";
