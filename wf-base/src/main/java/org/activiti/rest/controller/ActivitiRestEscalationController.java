a3531b3b87ff5a1e461ed96a70239bc5982a9e6c
LamronNu <olga.prylypko@gmail.com>
2015-08-12 14:50:18 +0300

    #658, - add column, - methods set-get-getAll-remove for EscalationRuleFunction

--- wf-base/src/main/java/org/activiti/rest/controller/ActivitiRestEscalationController.java
+++ wf-base/src/main/java/org/activiti/rest/controller/ActivitiRestEscalationController.java
@@ -2,6 +2,7 @@ package org.activiti.rest.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.wf.dp.dniprorada.base.dao.EscalationRuleFunctionDao;
 import org.wf.dp.dniprorada.base.model.EscalationRuleFunction;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Controller
@@ -17,16 +18,49 @@ import java.util.List;
 public class ActivitiRestEscalationController {
 
     private static final Logger log = Logger.getLogger(ActivitiRestEscalationController.class);
-
     @Autowired
-    private static EscalationRuleFunctionDao escalationRuleFunctionDao;
+    private EscalationRuleFunctionDao escalationRuleFunctionDao;
+
+    //util??
+    private void throwActivitiEx(String errorCode, String message, Throwable e)
+            throws ActivitiRestException {
+        if (isEmpty(message)) {
+            message = "error!";
+        }
+        ActivitiRestException newErr =
+                e == null ? new ActivitiRestException(errorCode, message)
+                        : new ActivitiRestException(errorCode, message, e);
+        newErr.setHttpStatus(HttpStatus.FORBIDDEN);//??
+        throw newErr;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || "".equals(str);
+    }
+
+    private void throwActivitiEx(String message, Throwable e)
+            throws ActivitiRestException {
+        throwActivitiEx("BUSINESS_ERR", message, e);
+    }
+
+    private void throwActivitiEx(String message)
+            throws ActivitiRestException {
+        throwActivitiEx("BUSINESS_ERR", message, null);
+    }
+
+    //---------------
+
+    private void throwActivitiEx(String errorCode, String message)
+            throws ActivitiRestException {
+        throwActivitiEx(errorCode, message, null);
+    }
 
     @RequestMapping(value = "/setEscalationRuleFunction", method = RequestMethod.GET)
     public EscalationRuleFunction setEscalationRuleFunction(
             @RequestParam(value = "nID", required = false) Long nID ,
             @RequestParam(value = "sName") String sName ,
-            @RequestParam(value = "sBeanHandler", required = false) String sBeanHandler,
-            HttpServletResponse response) throws ActivitiRestException {
+            @RequestParam(value = "sBeanHandler", required = false) String sBeanHandler)
+            throws ActivitiRestException {
 
         try {
             return escalationRuleFunctionDao.saveOrUpdate(nID, sName, sBeanHandler);
@@ -39,28 +73,43 @@ public class ActivitiRestEscalationController {
 
     @RequestMapping(value = "/getEscalationRuleFunction", method = RequestMethod.GET)
     public EscalationRuleFunction getEscalationRuleFunction(
-            @RequestParam(value = "nID") Long nID,
-            HttpServletResponse response) throws ActivitiRestException {
+            @RequestParam(value = "nID") Long nID) throws ActivitiRestException {
 
         try {
-            return escalationRuleFunctionDao.getById(nID);
+            EscalationRuleFunction ruleFunction = escalationRuleFunctionDao.getById(nID);
+            if (ruleFunction == null) {
+                throwActivitiEx("Record not found. No such ruleFunction with nID=" + nID);
+            }
+            return ruleFunction;
         } catch (Exception e){
             log.error("ex in controller!", e);
             throw new ActivitiRestException("ex in controller!", e);
         }
     }
 
+
     @RequestMapping(value = "/getEscalationRuleFunctions", method = RequestMethod.GET)
-     public List<EscalationRuleFunction> getEscalationRuleFunctions(
-            HttpServletResponse response) throws ActivitiRestException {
+    public List<EscalationRuleFunction> getEscalationRuleFunctions()
+            throws ActivitiRestException {
 
         try {
             return escalationRuleFunctionDao.getAll();
         } catch (Exception e){
-            log.error("ex in controller!", e);
             throw new ActivitiRestException("ex in controller!", e);
         }
     }
 
+    @RequestMapping(value = "/removeEscalationRuleFunction", method = RequestMethod.GET)
+    public void removeEscalationRuleFunction(
+            @RequestParam(value = "nID") Long nID) throws ActivitiRestException {
+
+        try {
+            escalationRuleFunctionDao.delete(nID);
+        } catch (EntityNotFoundException e) {
+            throwActivitiEx(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new ActivitiRestException("ex in controller!", e);
+        }
+    }
 
 }
