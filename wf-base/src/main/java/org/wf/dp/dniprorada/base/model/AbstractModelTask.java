257062ce850563f5ca34c09befba3bb2668f397f
bw <bw@bw-pc>
2015-08-17 16:04:44 +0300

    upload listiner optimisetion fix++

--- wf-base/src/main/java/org/wf/dp/dniprorada/base/model/AbstractModelTask.java
+++ wf-base/src/main/java/org/wf/dp/dniprorada/base/model/AbstractModelTask.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 import net.sf.jmimemagic.Magic;
 import net.sf.jmimemagic.MagicException;
