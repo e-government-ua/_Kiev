d80f943d2bb2b40cb412f58c825bea84fc7df790
serdyuk-alex <serdyk.alex@gmail.com>
2015-08-27 15:39:06 +0300

    #681 new stats tab inside service
    +add new tab and new service
    -remove call getService on each tab, put data to parent controller
    ServiceFormController
    -remove copy-paste tabs in instruction, legislation, questions...
    +use existing acitviti facade to call backend
    +remove copy-paste (which did same as activiti facade)

--- central-js/server/api/service/index.controller.js
+++ central-js/server/api/service/index.controller.js
@@ -1,16 +1,11 @@
-var request = require('request');
+'use strict';
+var _ = require('lodash');
+var activiti = require('../../components/activiti');
 
-module.exports.index = function(options, callback) {
-	var url = options.protocol+'://'+options.hostname+options.path+'/services/getService';
-	console.log(url);
-	return request.get({
-		'url': url,
-		'auth': {
-			'username': options.username,
-			'password': options.password
-		},
-		'qs': {
-			'nID': options.params.nID
-		}
-	}, callback);
+module.exports.index = function(req, res) {
+  activiti.sendGetRequest(req, res, '/services/getService?nID=' + req.query.nID);
+};
+
+module.exports.getServiceStatistics = function(req, res) {
+  activiti.sendGetRequest(req, res, '/services/getStatisticServiceCounts?nID_Service=' + req.params.nID);
 };
