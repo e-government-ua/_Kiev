e11547317ec1ab82d3ecad6a324cf9367a050084
dgroup <ydantezs@yandex.ru>
2015-08-28 21:33:07 +0300

    [#573] Daily update

--- wf-central/src/main/java/org/activiti/rest/controller/PlaceController.java
+++ wf-central/src/main/java/org/activiti/rest/controller/PlaceController.java
@@ -1,20 +1,22 @@
 package org.activiti.rest.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.common.base.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.wf.dp.dniprorada.base.dao.GenericEntityDao;
 import org.wf.dp.dniprorada.dao.PlaceDao;
+import org.wf.dp.dniprorada.dao.PlaceTypeDao;
 import org.wf.dp.dniprorada.dao.place.PlaceHierarchyRecord;
 import org.wf.dp.dniprorada.dao.place.PlaceHierarchyTree;
 import org.wf.dp.dniprorada.model.Place;
 import org.wf.dp.dniprorada.model.PlaceType;
+
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 /**
  * @author dgroup
@@ -22,17 +24,13 @@ import org.wf.dp.dniprorada.model.PlaceType;
  */
 @Controller
 public class PlaceController {
-    private static final Logger LOG = LoggerFactory.getLogger(PlaceController.class);
-
     private static final String JSON_TYPE = "Accept=application/json";
 
     @Autowired
     private PlaceDao placeDao;
 
     @Autowired
-    @Qualifier("placeTypeDao")
-    private GenericEntityDao<PlaceType> placeTypeDao;
-
+    private PlaceTypeDao placeTypeDao;
 
     @RequestMapping(value   = "/getPlacesTree",
                     method  = RequestMethod.GET, headers = { JSON_TYPE })
@@ -75,15 +73,51 @@ public class PlaceController {
             @RequestParam(value = "sID_UA",         required = false) String uaId,
             @RequestParam(value = "sNameOriginal",  required = false) String originalName
     ) {
-        // TODO need to understand which Dao should be used AbstractDaoXX or BaseXXX or GenericXX ...
         Place place = new Place();
         place.setId(placeId);
         place.setName(name);
         place.setPlaceTypeId(typeId);
         place.setUaId(uaId);
         place.setOriginalName(originalName);
-        // placeTypeDao.saveOrUpdate( place ); TODO and now we can't do it because of GenericEntityDao<PlaceType>
-        // Why we need a new DAO class which does not allow us to do basic operation (independent of type at all)?
+
+        if (positive(placeId)) {
+            placeDao.saveOrUpdate(place);
+
+        }  else if (isNotBlank(uaId)) {
+            Optional<Place> persistedPlace = placeDao.findBy("sID_UA", uaId);
+            if (persistedPlace.isPresent()) {
+                place.setId( persistedPlace.get().getId() );
+                placeDao.saveOrUpdate(place);
+            }
+        }
+    }
+
+
+    @RequestMapping(value   = "/removePlace",
+            method  = RequestMethod.POST, headers = { JSON_TYPE })
+    public  @ResponseBody void removePlace(
+            @RequestParam(value = "nID")    Long   placeId,
+            @RequestParam(value = "sID_UA") String uaId
+    ) {
+        if (positive(placeId)) {
+            placeTypeDao.delete(placeId);
+
+        } else if (isNotBlank(uaId)) {
+            Optional<Place> place = placeDao.findBy("sID_UA", uaId);
+            if (place.isPresent()) {
+                placeDao.delete(place.get());
+            }
+        }
+    }
+
+
+    @RequestMapping(value   = "/getPlaceTypes",
+            method  = RequestMethod.GET, headers = { JSON_TYPE })
+    public  @ResponseBody List<PlaceType> getPlaceTypes(
+            @RequestParam(value = "bArea") Boolean area,
+            @RequestParam(value = "bRoot") Boolean root
+    ) {
+        return placeTypeDao.getPlaceTypes(area, root);
     }
 
 
@@ -108,11 +142,18 @@ public class PlaceController {
         placeTypeDao.saveOrUpdate( new PlaceType(placeTypeId, name, order, area, root) );
     }
 
+
     @RequestMapping(value   = "/removePlaceType",
                     method  = RequestMethod.POST, headers = { JSON_TYPE })
     public  @ResponseBody void removePlaceType(
             @RequestParam(value = "nID") Long placeTypeId
     ) {
-        placeTypeDao.delete(placeTypeId);
+        placeTypeDao.delete( placeTypeId );
+    }
+
+
+
+    private static boolean positive(Long value){
+        return value!=null && value > 0;
     }
 }
