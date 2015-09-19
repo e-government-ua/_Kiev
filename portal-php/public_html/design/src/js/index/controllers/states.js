34b17282b7cf71d42638f6c594b01f23c4a061aa
sybase <bw@pb.ua>
2015-09-19 22:39:49 +0300

    clear old

--- portal-php/public_html/design/src/js/index/controllers/states.js
+++
@@ -1,5 +0,0 @@
-define('state/index/controller', ['angularAMD'], function (angularAMD) {
-	angularAMD.controller('IndexController', ['$scope', 'catalog', function ($scope, catalog) {
-		$scope.catalog = catalog;
-    }]);
-});
