5a82ed384ea28e8bc9382f03d5795444b60a933b
serdyuk-alex <serdyk.alex@gmail.com>
2015-09-26 12:27:19 +0300

    #828 add motion for required

--- central-js/client/app/common/services/field.motion.service.js
+++ central-js/client/app/common/services/field.motion.service.js
@@ -2,51 +2,65 @@ angular.module('app').service('FieldMotionService', ['MarkersFactory', FieldMoti
 
 function FieldMotionService(MarkersFactory) {
 
-  function getData() {
-    return {
-      showOnNotEmpty: MarkersFactory.grepByPrefix('motion', 'ShowFieldsOnNotEmpty_'),
-      showOnCondition: MarkersFactory.grepByPrefix('motion', 'ShowFieldsOnCondition_')
+  this.isFieldMentioned = {
+    'in': function(fieldId, prefix) {
+        return grepByPrefix(prefix).some(function(entry) {
+          return _.contains(entry.aField_ID, fieldId);
+        });
+    },
+    inShow: function(fieldId) {
+      return this.in(fieldId, 'ShowFieldsOn');
+    },
+    inRequired: function(fieldId) {
+      return this.in(fieldId, 'RequiredFieldsOn');
     }
-  }
-
-  this.isFieldMentioned = function(fieldId) {
-    return _.values(getData()).some(function(entry) {
-      return entry.some(function(showEntry) {
-        return _.contains(showEntry.aField_ID, fieldId);
-      });
-    });
   };
 
   this.isFieldVisible = function(fieldId, formData) {
-    var data = getData();
-    var b1 = data.showOnNotEmpty.some(function(e) {
+    var showOnNotEmpty = grepByPrefix('ShowFieldsOnNotEmpty_');
+    var showOnCondition = grepByPrefix('ShowFieldsOnCondition_');
+    var b1 = showOnNotEmpty.some(function(e) {
       return formData[e.sField_ID_s]
         && $.trim(formData[e.sField_ID_s].value)
         && _.contains(e.aField_ID, fieldId);
     });
-    return b1 || data.showOnCondition.some(function(entry) {
-      if (!_.contains(entry.aField_ID, fieldId)) return false;
-      var toEval = entry.sCondition.replace(/\[(\w+)]/g, function(str, alias) {
-        var fId = entry.asID_Field[alias];
-        if (!fId) alert('Cant resolve original fieldId by alias:' + alias);
-        var result = '';
-        if (formData[fId] && formData[fId].value)
-          result = formData[fId].value.replace(/'/g, "\\'");
-        switch(alias.charAt(0)) {
-          case 's': result = "'" + result + "'"; break;
-          case 'n': result = result ? parseFloat(result) : 0; break;
-          default: alert('invalid alias format, alias:' + alias);
-        }
-        return result;
-      });
-        try {
-          return eval(toEval);
-        } catch (e) {
-          alert('OnCondition expression error\n' + e.name + '\n' + e.message
-            + '\nexpression:' + entry.sCondition
-            + '\nresolved expression:' + toEval);
-          throw e;
-        }
+    return b1 || showOnCondition.some(function(entry) {
+      return evalCondition(entry, fieldId, formData);
     });
+  };
+
+  this.isFieldRequired = function(fieldId, formData) {
+    return grepByPrefix('RequiredFieldsOnCondition_').some(function(entry) {
+      return evalCondition(entry, fieldId, formData);
+    })
+  };
+
+  function evalCondition(entry, fieldId, formData) {
+    if (!_.contains(entry.aField_ID, fieldId)) return false;
+    var toEval = entry.sCondition.replace(/\[(\w+)]/g, function(str, alias) {
+      var fId = entry.asID_Field[alias];
+      if (!fId) alert('Cant resolve original fieldId by alias:' + alias);
+      var result = '';
+      if (formData[fId] && formData[fId].value)
+        result = formData[fId].value.replace(/'/g, "\\'");
+      switch(alias.charAt(0)) {
+        case 's': result = "'" + result + "'"; break;
+        case 'n': result = result ? parseFloat(result) : 0; break;
+        default: alert('invalid alias format, alias:' + alias);
+      }
+      return result;
+    });
+    try {
+      return eval(toEval);
+    } catch (e) {
+      alert('OnCondition expression error\n' + e.name + '\n' + e.message
+        + '\nexpression:' + entry.sCondition
+        + '\nresolved expression:' + toEval);
+      throw e;
+    }
+  }
+
+  function grepByPrefix(prefix) {
+    return MarkersFactory.grepByPrefix('motion', prefix);
   }
 }
