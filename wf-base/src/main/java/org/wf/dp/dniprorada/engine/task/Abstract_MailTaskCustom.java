3cb184ee6de193f80b437e1aab9b866052f79738
Ольга <olga.kuzminova87@gmail.com>
2015-10-07 02:02:55 +0400

    remove logs

--- wf-base/src/main/java/org/wf/dp/dniprorada/engine/task/Abstract_MailTaskCustom.java
+++ wf-base/src/main/java/org/wf/dp/dniprorada/engine/task/Abstract_MailTaskCustom.java
@@ -113,7 +113,6 @@ public abstract class Abstract_MailTaskCustom implements JavaDelegate {
         if (textStr == null) {
             return null;
         }
-        LOG.info("begin textStr: " + textStr);
         String textWithoutTags = replaceTags_LIQPAY(textStr, execution);
 
         textWithoutTags = new MVSDepartmentsTagUtil().replaceMVSTagWithValue(textWithoutTags);
@@ -238,18 +237,15 @@ public abstract class Abstract_MailTaskCustom implements JavaDelegate {
         if (matcher.find()) {
             matcher = TAG_sPATTERN_CONTENT_CATALOG.matcher(textStr);
             List<String> aPreviousUserTask_ID = getPreviousTaskId(execution);
-            LOG.info("aPreviousUserTask_ID: " + aPreviousUserTask_ID);
             Map<String, FormProperty> mProperty = new HashMap<String, FormProperty>();
             loadPropertiesFromTasks(execution, aPreviousUserTask_ID, mProperty);
             while (matcher.find()) {
                 String tag_Payment_CONTENT_CATALOG = matcher.group();
-                LOG.info("tag_Payment_CONTENT_CATALOG: " + tag_Payment_CONTENT_CATALOG);
                 if (!tag_Payment_CONTENT_CATALOG.startsWith(TAG_Function_AtEnum)) {
                     String prefix;
                     Matcher matcherPrefix = TAG_PATTERN_DOUBLE_BRACKET.matcher(tag_Payment_CONTENT_CATALOG);
                     if (matcherPrefix.find()) {
                         prefix = matcherPrefix.group();
-                        LOG.info("prefix: " + prefix);
                         Matcher matcherText = TAG_PATTERN_TEXT.matcher(prefix);
                         if (matcherText.find()) {
                             String form_ID = matcherText.group();
@@ -398,7 +394,6 @@ public abstract class Abstract_MailTaskCustom implements JavaDelegate {
         List<String> resIDs = new LinkedList<String>();
 
         for (FlowElement flowElement : execution.getEngineServices().getRepositoryService().getBpmnModel(ee.getProcessDefinitionId()).getMainProcess().getFlowElements()) {
-            LOG.info("flowElement.getClass " + flowElement.getClass());
             if (flowElement instanceof UserTask) {
                 UserTask userTask = (UserTask) flowElement;
                 LOG.info("Checking user task with ID: " + userTask.getId());
